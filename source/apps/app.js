c79ec8c01e807589a6ef93b8120f70dbcc43b5c7
Dave Freeman <sugardave@gmail.com>
2013-05-01 22:22:16 -0500

    switched to master branches

--- source/apps/app.js
+++ source/apps/app.js
@@ -1,7 +1,12 @@
 enyo.kind({
 	name: "enyojs.Gallery.Application",
-	kind: "enyo.Application",
-	controllers: [{
+	kind: enyo.Application,
+	controllers: [
+		{name: "router", kind: enyo.Router, routes: [
+			{handler: "updateView", context: "app", default: true}
+		]},
+		{name: "cards", kind: enyo.ArrayController},
+	{
 		name: "message",
 		kind: "enyojs.Gallery.MessageController"
 	}, {
@@ -9,9 +14,22 @@ enyo.kind({
 		kind: "enyojs.Gallery.MessagesController"
 	}],
 	view: "enyojs.Gallery.MainView",
+	create: function() {
+		this.inherited(arguments);
+		this.log(this.$.router.location());
+	},
 	addRecord: function (sender, event) {
+		this.log(this.$.router.location());
 		var data = this.controllers.message.get("data");
 		var messages = this.controllers.messages;
 		messages.add({message: data});
+	},
+	updateView: function(inPath) {
+		switch (inPath) {
+			case "/":
+			default:
+				this.log("SHOW DEFAULT VIEW");
+				break;
+		}
 	}
 });
